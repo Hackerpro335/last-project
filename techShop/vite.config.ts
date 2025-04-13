50e9738314cbc5889f5ad9335d96b4a61c0a3094
Hackerpro335 <iurewi4.tix@gmail.com>
2025-04-13 14:35:27 +0300

    still working

--- techShop/vite.config.ts
+++ techShop/vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@app': '/src/app',
+      '@pages': '/src/pages',
+      '@widgets': '/src/widgets',
+      '@features': '/src/features',
+      '@entities': '/src/entities',
+      '@shared': '/src/shared'
+    }
+  },
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
+        changeOrigin: true
       }
     }
-  }
-})
+  },
+});
